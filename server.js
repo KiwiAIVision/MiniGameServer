95bb8d1a6889b1c7903ecf2e1fe76295eccad889
hughli <hughli@zego.im>
2024-02-01 16:09:41 +0800

    屏蔽重定向

--- server.js
+++ server.js
@@ -19,11 +19,11 @@ const baseCurrency = 0;
 
 
 // 启用HTTPS到HTTP的重定向
-app.use(redirect({
-  from: 'http://',
-  to: 'https://',
-  permanent: true // 设置为true以在浏览器中产生301永久重定向
-}));
+// app.use(redirect({
+//   from: 'http://',
+//   to: 'https://',
+//   permanent: true // 设置为true以在浏览器中产生301永久重定向
+// }));
 
 
 app.use(cors({
