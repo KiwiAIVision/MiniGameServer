4c3a4337c66a0bb816e180344ca1f55b88b49df3
hughli <hughli@zego.im>
2024-02-01 16:33:08 +0800

    修改请求头

--- server.js
+++ server.js
@@ -29,15 +29,7 @@ const baseCurrency = 0;
 app.use(cors({
     origin: "*",
     methods: ["GET","POST", "OPTION"]
-}),(req, res, next) => {
-        res.set('X-Content-Type-Options', 'nosniff');
-        res.set('Cache-Control', 'no-store');
-        res.set('X-XSS-Protection', '1; mode=block');
-        res.set('Strict-Transport-Security', '3600');
-        res.set('Content-Security-Policy','upgrade-insecure-requests);
-        next();
-    }
-)
+}))
 // app.use( ( request , response , next ) => {
 //     response.header( 'Access-Control-Allow-Origin' , '*') // 跨域最重要的一步 设置响应头
 // 	response.header('Content-Security-Policy','upgrade-insecure-requests');
